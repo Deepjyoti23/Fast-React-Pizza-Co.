01e558847a2b4be271c9af2ed05d3fafb0dce680
Deepjyoti23 <deepjyoti0401@gmail.com>
2023-09-05 18:21:31 +0530

    add user

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -33,6 +34,8 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((state) => state.user.username);
+
   // const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
@@ -49,18 +52,18 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input className="input grow" type="text" name="customer" defaultValue={username} required />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
           <div className="grow">
             <input className="input w-full " type="tel" name="phone" required />
-          {formErrors?.phone && (
-            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
-              {formErrors.phone}
-            </p>
-          )}
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
@@ -105,7 +108,7 @@ export async function action({ request }) {
   // console.log(request)
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+  // console.log(data);
 
   const order = {
     ...data,
@@ -113,7 +116,7 @@ export async function action({ request }) {
     priority: data.priority === 'on',
   };
 
-  console.log(order);
+  // console.log(order);
 
   const errors = {};
   if (!isValidPhone(order.phone))
